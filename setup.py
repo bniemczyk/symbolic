b116554ba5fcd2df84801018bce637638e6f2de1
Brandon Niemczyk <brandon.niemczyk@hp.com>
2012-11-19 09:55:51 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( \
+  name='symbolic', \
+  version='git', \
+  description='symbolic mathematics for python', \
+  author='Brandon Niemczyk', \
+  author_email='brandon.niemczyk@gmail.com', \
+  url='http://github.com/bniemczyk/symbolics', \
+  packages=['symbolic'] \
+  )
