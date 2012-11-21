a12c30fe85c523b9c2965f4216a9b51bd768d01d
Brandon Niemczyk <brandon.niemczyk@hp.com>
2012-11-21 16:05:47 -0600

    reset version to git

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup( \
   name='symath', \
-  version='0.1.11', \
+  version='git', \
   description='symbolic mathematics for python', \
   author='Brandon Niemczyk', \
   author_email='brandon.niemczyk@gmail.com', \
