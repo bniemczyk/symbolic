eeb2b033f85e1a2710df21f8a0d68f6ccbcbb8d8
Brandon Niemczyk <brandon.niemczyk@hp.com>
2012-11-23 12:28:22 -0600

    incremented version #

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ numpy
 
 setup( \
   name='symath', \
-  version='0.1.15', \
+  version='0.1.16', \
   description='symbolic mathematics for python', \
   author='Brandon Niemczyk', \
   author_email='brandon.niemczyk@gmail.com', \
