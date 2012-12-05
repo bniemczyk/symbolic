ebb7bd5a87a19a180af43dbb94ca8a5b3a67cc1f
Brandon Niemczyk <brandon.niemczyk@hp.com>
2012-12-05 03:36:11 +0000

    added some test cases for edit_distance

--- tests/algorithms.py
+++ tests/algorithms.py
@@ -11,10 +11,13 @@ class TestAlgorithms(unittest.TestCase):
   def test_edit_distance(self):
     from symath.algorithms.editdistance import edit_distance
     x,y = symath.symbols('x y')
+    a,b = symath.wilds('a b')
     self.assertEqual(edit_distance(x(x, y, x), y(x, x, x)), 2)
     self.assertEqual(edit_distance(x(y, x), x(y, y, x)), 1)
     self.assertEqual(edit_distance(x(y, x), x(x)), 1)
     self.assertEqual(edit_distance(x(y, y, x), x(x)), 2)
+    self.assertEqual(edit_distance(a, x(y, y)), 0)
+    self.assertEqual(edit_distance(a(x, x), x(y, x)), 1)
 
 if __name__ == '__main__':
   unittest.main()
